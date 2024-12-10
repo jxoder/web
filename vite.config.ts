591fc44ff5da1815c7c6b477f3e6bc3fd47ca450
namoo.jang <chrow16@naver.com>
2024-12-10 21:23:26 +0900

    빌드시 발생되는 경고 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 3000,
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: {
